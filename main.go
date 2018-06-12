b7e3a36a667053210e0fb3462ef990eba5467855
luolulu <luo@LUOdeMacBook-Pro.local>
2018-06-11 23:18:06 -0500

    Authentication use JWT

--- main.go
+++ main.go
@@ -11,6 +11,9 @@ import (
 	"strconv"
 
 	"cloud.google.com/go/storage"
+	"github.com/auth0/go-jwt-middleware"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gorilla/mux"
 	"github.com/pborman/uuid"
 	elastic "gopkg.in/olivere/elastic.v3"
 )
@@ -27,6 +30,8 @@ type Post struct {
 	Location Location `json:"location"`
 	Url      string   `json:"url"`
 }
+
+var mySigningKey = []byte("secret")
 
 func main() {
 	// Create a client
@@ -62,8 +67,22 @@ func main() {
 	}
 
 	fmt.Println("started-service")
-	http.HandleFunc("/post", handlerPost)
-	http.HandleFunc("/search", handlerSearch)
+
+	r := mux.NewRouter()
+
+	var jwtMiddleware = jwtmiddleware.New(jwtmiddleware.Options{
+		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+			return mySigningKey, nil
+		},
+		SigningMethod: jwt.SigningMethodHS256,
+	})
+
+	r.Handle("/post", jwtMiddleware.Handler(http.HandlerFunc(handlerPost))).Methods("POST")
+	r.Handle("/search", jwtMiddleware.Handler(http.HandlerFunc(handlerSearch))).Methods("GET")
+	r.Handle("/login", http.HandlerFunc(loginHandler)).Methods("POST")
+	r.Handle("/signup", http.HandlerFunc(signupHandler)).Methods("POST")
+
+	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
@@ -73,6 +92,9 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content_Type", "apllication/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	user := r.Context().Value("user")
+	claims := user.(*jwt.Token).Claims
+	username := claims.(jwt.MapClaims)["username"]
 
 	r.ParseMultipartForm(32 << 20)
 
@@ -82,7 +104,7 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 	lon, _ := strconv.ParseFloat(r.FormValue("lon"), 64)
 
 	p := &Post{
-		User:    "1111",
+		User:    username.(string),
 		Message: r.FormValue("message"),
 		Location: Location{
 			Lat: lat,
@@ -94,8 +116,8 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 
 	file, _, err := r.FormFile("image")
 	if err != nil {
-		http.Error(w, "GCS is not setup", http.StatusInternalServerError)
-		fmt.Printf("GCS is not setup %v\n", err)
+		http.Error(w, "image is not available", http.StatusInternalServerError)
+		fmt.Printf("image is not available %v\n", err)
 		panic(err)
 	}
 	defer file.Close()
@@ -181,7 +203,7 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL      = "http://35.232.46.215:9200"
+	ES_URL      = "http://35.193.158.31:9200"
 	BUCKET_NAME = "post-images-206421"
 )
 
